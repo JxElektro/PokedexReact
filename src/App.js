b7a6fc6f25ee77e1d83f56fd4471bd2d0d06765f
Jhen Nuñez <jxelektro@gmail.com>
2022-12-13 13:33:17 -0300

    poke

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
+import styled from 'styled-components';
+
 
 function App() {
+  const [pokemon, setPokemon] = useState(null);
+  const [input, setInput] = useState('');
+
+  const getPokemon = async (name) => {
+    const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`);
+    // Pass the response data to the setPokemon function
+    setPokemon(response.data);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Pokedex</h1>
+      <input type="text" onChange={(e) => setInput(e.target.value)} />
+      <button onClick={() => getPokemon(input)}>Buscar Pokemon</button>
+
+      {pokemon ? (
+        <div>
+          <h2>{pokemon.name}</h2>
+          <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+          
+        </div>
+      ) : (
+        <p>No Pokemon Found</p>
+      )}
     </div>
   );
 }
 
+
 export default App;
+
+// Cada vez que escribo un numero se pone la pagina en blanco
